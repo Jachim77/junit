d3234be2c0c9b40ece38891b5682ddb7afa480a3
Jan Michal <jan.michal@email.cz>
2023-03-27 16:20:23 +0200

    verze 1

--- src/main/java/com/engeto/Main.java
+++ src/main/java/com/engeto/Main.java
@@ -1,7 +1,12 @@
 package com.engeto;
 
+import com.engeto.calculator.Calculator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+
 }
--- src/main/java/com/engeto/calculator/Calculator.java
+++ src/main/java/com/engeto/calculator/Calculator.java
@@ -1,5 +1,42 @@
 package com.engeto.calculator;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Calculator {
+
+    public Calculator() {
+    }
+
+    public long add(int a, int b){
+        return a+b;
+    }
+    public long subtract(int a, int b){
+        return a-b;
+    }
+    public long multiply(int a, int b){
+        return a*b;
+    }
+    public double divide(int a, int b){
+        if(b==0){
+            throw new ArithmeticException();
+        }
+        return (double) a/b;
+    }
+    public double squareRoot(int a){
+        if(a<0) {
+            throw new ArithmeticException();
+        }
+        return (double) sqrt(a);
+    }
+    public double square(int a) {
+        return (double) pow(a,2);
+    }
+    public double areaOfCircle(int a){
+        if (a<0) {
+            throw new ArithmeticException();
+        }
+        return (double) (3.141593 * (pow(a,2)));
+    }
 
 }
